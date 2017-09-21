e4697ebc4f608fe33a1800505775f955f2ad1f19
Clauber  Camilo <Clauber@Claubers-MacBook-Pro.local>
2017-09-21 08:56:09 -0300

    Clear Configs

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,10 +1,3 @@
 export default {
-    API_URL: 'http://api.enora.tempspace.no/v1/admin',
-    LEAGUE_PANEL_URL: 'http://&slug;.enoraleague.tempspace.no',
 
-    CLIENT_GRANT_TYPE: 'password',
-    CLIENT_ID: '2',
-    CLIENT_SECRET: 'QCNcmVEXRgTbjEmPQzbi0OWTceDOyqhvrkOXwxp7',
-
-    IMG_PROVIDER: 'https://s3.amazonaws.com/leagues-uploads'
 }
